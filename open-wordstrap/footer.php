8e36b3b93d6261d0134ff76172ea23ceb775c079
Steve King <steve@stevenking.com.au>
2015-03-05 16:02:50 +1100

    Adding mention into footer file based on original theme.

--- open-wordstrap/footer.php
+++ open-wordstrap/footer.php
@@ -1,28 +1,28 @@
 <?php 
 	
-	/** Footer.php
+	/** Open Wordstrap
+	 * Footer.php
 	 * 
+	 * This is the basic footer for the theme.
 	 */
 
 ?>	
-	
-	
     <div class="wsContentFooter">
 	
-		<?php if ( is_active_sidebar( 'wsfootermenu' ) ) : ?>
-	<div class="wsFooterMenu">
-		<?php dynamic_sidebar( 'wsfootermenu' ); ?>
-	</div>
-	<?php endif; ?>
-          
-          <p><a href='https://github.com/steveathon/Open-Wordstrap' target='_blank'>Fork this Wordpress theme.</a><br /></p>
-          
+	<?php if ( is_active_sidebar( 'wsfootermenu' ) ) : ?>
+		<div class="wsFooterMenu">
+			<?php dynamic_sidebar( 'wsfootermenu' ); ?>
+		</div>
+		<?php endif; ?>
+	          <p><a href='https://github.com/steveathon/Open-Wordstrap' target='_blank'>Fork this Wordpress theme.</a><br /></p>
+	    <small>Based on theme by <a href='https://twitter.com/mdo' target='_blank'>@mdo</a></small>
     </div>
           
-	</div>
+</div>
 	
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 	<?php wp_footer(); ?>
+		
 </body>
 </html>
