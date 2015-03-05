9da413d232b1ac8ce8fbfc502bc4b8c5499f7bfa
Steve King <steve@stevenking.com.au>
2015-03-05 15:31:07 +1100

    Pushing up a fix on the single content panel.

--- single.php
+++ single.php
@@ -1,10 +1,10 @@
 <?php 
 	
-	/**
-	 * single.php
+	/** Open Wordstrap single.php
+	 * 
+	 * Provides content template for a single post within the theme.
+	 * 
 	 */
-
-
 	get_header(); ?>
 	
 	<div class="row">
@@ -15,12 +15,12 @@
 					<h2 class="blog-post-title"><?php the_title(); ?></h2>
 					<?php the_content(); ?>
 					<?php edit_post_link( __( '<i class="fa fa-pencil-square-o"></i>' ) ) ?>
+					<p>Written by: <?php the_author_posts_link(); ?></p>
 				</div>	                       
 			</div>
 		</div>
 		<?php get_sidebar(); ?>
 	</div>
 <?php 
-
 	get_footer(); 
-	?>
+?>
