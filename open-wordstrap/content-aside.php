e85bc25b8a0edf6b80d6ac47783fe513357e8f2f
Steve King <steve@stevenking.com.au>
2015-03-06 16:53:13 +1100

    Updating the content / content links and single with the updated blog layout. Adding styling to match.

--- open-wordstrap/content-aside.php
+++ open-wordstrap/content-aside.php
@@ -1,14 +1,43 @@
 <?php 
 	?>
 	
-  <div class="wsMiniPost">
-			<div id="wsPost-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-				<div>
-					<h2 class="WSPosTitle"><?php the_title(); ?></h2>
-					<?php the_content(); ?>
-					<?php edit_post_link( __( '<i class="fa fa-pencil-square-o"></i>' ) ) ?>
-				</div>	                       
+  <div class="WSMiniPost">
+	<div id="WSPost-<?php the_ID(); ?>" <?php post_class('WSPost'); ?>>
+			<h2 class="entry-title">
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" rel="bookmark">
+			<?php the_title(); ?></a></h2>
+		
+			<div class="WSPostMeta">
+				<ul>
+					<li class='WSAuthorImage'>
+						<a href="#!">
+						<img class="img-responsive img-circle" src='<?php 
+							echo get_gravatar($authordata->user_email,25); ?>' /></a>
+					</li>
+					<li class="WSAuthor">written by 
+						<a class="WSAuthorName" href="<?php echo get_author_link( false, $authordata->ID, $authordata->user_nicename ); ?>" title="<?php printf( 'View all posts by %s', $authordata->display_name ); ?>"><?php the_author(); ?></a>
+					</li>                        
+					<li class='WSPostPublishDate'>published on 
+						<abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>">
+						<?php the_time( get_option( 'date_format' ) ); ?></abbr>
+					</li>
+					<?php edit_post_link( __( 'Edit', 'open-wordstrap' ), "<li class=\"edit-link\">", "</li>" ) ?>
+				                        </ul>
 			</div>
+			
+			<div class='WSPostBody'>
+				<?php the_excerpt(); ?>
+			</div>                  
+			
+			<div class='WSPostSummary'>
+				<ul>
+					<li><?php echo get_the_category_list(', '); ?></li>
+					<li><?php the_tags(); ?></li>
+					<li><?php comments_popup_link();?></li>
+					<li><?php edit_post_link(); ?></li>
+				</ul>
+			</div>
+					
+		</div>
 	
 	</div>
--- open-wordstrap/single.php
+++ open-wordstrap/single.php
@@ -9,14 +9,45 @@
 	
 	<div class="row">
 		<div class="col-sm-8 wsBlog-main">
-			<div id="wsPost-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<div id="wsPost-<?php the_ID(); ?>" <?php post_class('WSPost'); ?>>
 				<?php the_post(); ?>
-				<div>
-					<h2 class="blog-post-title"><?php the_title(); ?></h2>
-					<?php the_content(); ?>
-					<?php edit_post_link( __( '<i class="fa fa-pencil-square-o"></i>' ) ) ?>
-					<p>Written by: <?php the_author_posts_link(); ?></p>
-				</div>	                       
+			<div id="WSPost-<?php the_ID(); ?>" <?php post_class('WSPost'); ?>>
+			<h2 class="entry-title">
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" rel="bookmark">
+			<?php the_title(); ?></a></h2>
+		
+			<div class="WSPostMeta">
+				<ul>
+					<li class='WSAuthorImage'>
+						<a href="#!">
+						<img class="img-responsive img-circle" src='<?php 
+							echo get_gravatar($authordata->user_email,25); ?>' /></a>
+					</li>
+					<li class="WSAuthor">written by 
+						<a class="WSAuthorName" href="<?php echo get_author_link( false, $authordata->ID, $authordata->user_nicename ); ?>" title="<?php printf( 'View all posts by %s', $authordata->display_name ); ?>"><?php the_author(); ?></a>
+					</li>                        
+					<li class='WSPostPublishDate'>published on 
+						<abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>">
+						<?php the_time( get_option( 'date_format' ) ); ?></abbr>
+					</li>
+					<?php edit_post_link( __( 'Edit', 'open-wordstrap' ), "<li class=\"edit-link\">", "</li>" ) ?>
+				                        </ul>
+			</div>
+			
+			<div class='WSPostBody'>
+				<?php the_excerpt(); ?>
+			</div>                  
+			
+			<div class='WSPostSummary'>
+				<ul>
+					<li><?php echo get_the_category_list(', '); ?></li>
+					<li><?php the_tags(); ?></li>
+					<li><?php comments_popup_link();?></li>
+					<li><?php edit_post_link(); ?></li>
+				</ul>
+			</div>
+					
+		</div>  
 			</div>
 		</div>
 		<?php get_sidebar(); ?>
